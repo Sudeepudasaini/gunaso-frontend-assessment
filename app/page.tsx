c6311797ee6631537d859e72ba38e2f6369a01ab
Sudeeppudasaini <heroclan82@gmail.com>
2025-09-04 22:39:16 +0545

    added

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,4 @@
 
-// "use client";
-
-// import { Button } from "@carbon/react";
-
-// export default function HomePage() {
-//   return (
-//     <main style={{ padding: "2rem" }}>
-//       <h1>Gunaso System Setup</h1>
-//       <p>This is my F0 setup page demonstrating Carbon theming.</p>
-
-     
-//      <Button kind="primary" className="green-button" style={{ marginTop: "1rem" }}>
-//         Carbon Button Test
-//       </Button>
-
-//     </main>
-//   );
-// }
-
-
-
-
 "use client";
 
 import { Button } from "@carbon/react";
